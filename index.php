d7e382b11afd8181b174270adc4d0f09f1510220
Votre Nom <amdy@dofbi0.2>
2020-06-24 11:51:43 +0000

    merge3

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ echo "frere diote naa code yii ";
 echo "exemple de merge  dans ghitub gay am foofou";
 echo " voi ci mon deuxieme exemple frére";
 echo " voici mon deuxieme  de merge exemple frére";
+echo " voici mon toixieme  exemple  de merge exemple frére";
+
 
 
 
